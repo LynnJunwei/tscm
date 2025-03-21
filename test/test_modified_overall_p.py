2b3492db1dfe22944a8d673e70e368520d60d38a
LIN Junwei <junwei.lin@connect.polyu.hk>
2025-03-21 14:33:18 +0800

    new test file

--- test/test_modified_overall_p.py
+++ test/test_modified_overall_p.py
@@ -10,7 +10,7 @@ from tscm.core import TSCMObject
 
 
 if __name__ == '__main__':
-    local_ts_df = pd.read_csv('test_case/TSV_7-5-6_1.csv', index_col=0)
+    local_ts_df = pd.read_csv('test_case/TSV_3.csv', index_col=0)
 
     human_config = HumanConfig(sex='male', met=1, clo=0.5)
     overall_sensation_config_origin = OverallSensationConfig(external_smooth=True, original_model=True,
@@ -25,25 +25,26 @@ if __name__ == '__main__':
     whole_ts_origin_model = TSCMObject(local_sensation=local_ts_df,
                                        overall_sensation_config=overall_sensation_config_origin,
                                        output='os')
-    whole_ts_origin_model.run(20)
+    whole_ts_origin_model.run(5)
     whole_ts_origin = whole_ts_origin_model.overall_sensation
     model_num = whole_ts_origin_model.model_num
 
     whole_ts_modified_model = TSCMObject(local_sensation=local_ts_df,
                                          overall_sensation_config=overall_sensation_config_modified,
                                          output='os')
-    whole_ts_modified_model.run(20)
+    whole_ts_modified_model.run(5)
     whole_ts_modified = whole_ts_modified_model.overall_sensation
 
-    whole_ts_df = pd.concat([whole_ts_origin, whole_ts_modified], axis=1, keys=['origin', 'modified'])
-
+    # whole_ts_df = pd.concat([whole_ts_origin, whole_ts_modified], axis=1, keys=['origin', 'modified'])
+    print(whole_ts_origin)
     plt.figure(layout='constrained')
-    sns.lineplot(whole_ts_df, lw=3)
+    # sns.lineplot(whole_ts_df, lw=3)
+    sns.lineplot(whole_ts_origin, lw=4, label='overall', color='red')
     sns.lineplot(local_ts_df)
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.show()
 
     sns.lineplot(model_num, lw=3)
-    plt.show()
+    # plt.show()
 
 
